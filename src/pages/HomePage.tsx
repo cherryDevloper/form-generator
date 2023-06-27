97bc6a5c00ea73be9d681895e1ddc0aec0c9d2fb
sharareDev <sharareshaddev@gmail.com>
2023-06-27 16:17:52 +0330

    feat:added a create form button in home screen

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import Layout from '../components/Layout';
-
+import { Button } from '@chakra-ui/react';
 const HomePage = () => {
-  return <Layout>HomePage</Layout>;
+  return (
+    <Layout>
+      <Button
+        colorScheme="teal"
+        variant="solid"
+      >
+        Create Form
+      </Button>
+    </Layout>
+  );
 };
 
 export default HomePage;
