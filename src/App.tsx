7207e94a3d792f0bc6122a1c848acbe60751a4fc
sharareDev <sharareshaddev@gmail.com>
2023-06-27 16:23:54 +0330

    added react router dom for routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import HomePage from './pages/HomePage';
 import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import FormGenratorPage from './pages/FormGenratorPage';
 function App() {
   return (
     <ChakraProvider>
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={<HomePage />}
+          />
+          <Route
+            path="/form-generator"
+            element={<FormGenratorPage />}
+          />
+        </Routes>
+      </Router>
       <HomePage />
     </ChakraProvider>
   );
