212d78fad9738782d05b04f3a1bfb3666889a889
sharareDev <sharareshaddev@gmail.com>
2023-06-29 19:10:48 +0330

    fix:fixed type of setElements

--- src/components/Checkbox/Checkbox.type.ts
+++ src/components/Checkbox/Checkbox.type.ts
@@ -1,8 +1,11 @@
+import { Dispatch } from 'react';
+import { Element } from '../FormBuilder/FormBuilder.types';
+
 export interface CheckboxProps {
   label?: string;
   defaultValue?: boolean;
   name: string;
-  setElements: any;
+  setElements: Dispatch<React.SetStateAction<Element[]>>;
   onChange: () => void;
   choices: choicesType[];
 }
