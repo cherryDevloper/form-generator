2c9f18ebd25c919fa1844b6cda7b98557b968fbf
sharareDev <sharareshaddev@gmail.com>
2023-07-04 15:43:08 +0330

    remove spin componet , instaed used chakra ui spinner

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, Spinner } from '@chakra-ui/react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { QueryClientProvider } from 'react-query';
 import { Suspense } from 'react';
 import { routes } from './constants/routes';
 import queryClient from './api/queryClient';
-import Spin from './components/Spin';
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <Router>
         <ChakraProvider>
-          <Suspense fallback={<Spin />}>
+          <Suspense
+            fallback={
+              <Spinner
+                size="xl"
+                color="blue.500"
+                thickness="2px"
+              />
+            }
+          >
             <Routes>
               {routes.map((route, index) => (
                 <Route
